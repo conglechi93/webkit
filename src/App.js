3c1291576092f5cdeab8c9e94dfd90301fbf3251
conglechi93 <66486095+conglechi93@users.noreply.github.com>
2022-11-19 16:52:36 +0700

    up source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import backgroundImg from './images/background.png'
+import logoImg from './images/logo.png';
+import {Button, Input } from 'antd';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="main" style={{ backgroundImage: `url(${backgroundImg})`}}>
+        <div>
+          <header>
+            <div className="container">
+              <div className="row">
+                <a className="logo text-center">
+                  <img className="img-responsive" data-src="images/logo.png" src={logoImg} alt=""/>
+                </a>
+              </div>
+            </div>
+          </header>
+          <section className='player'>
+            <div class="container">
+              <div className='row'>
+                <div className='form_style'>
+                  <div className='tab-content'>
+                    <div className='form-group'>
+                      <Button className='btn-login'></Button>
+                      <Button className='btn-register'></Button>
+                      <div className='tab-pane'>
+                        <span className='ant-input-affix-wrapper'>
+                          <Input className='input-default' placeholder='Tên dăng nhập'></Input>
+                        </span>
+                        
+                        <Input.Password maxLength={30} placeholder='Mật khẩu' type='password' className='ant-input-affix-wrapper'/>
+                      </div>
+                    </div>
+                    <div className='form-group btn_form'>
+                      <Button className='btn-login'></Button>
+                      <Button className='btn-register'></Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
